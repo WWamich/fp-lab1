2ab5ebe022edb860a04b615f3cbf4aac68b8890b
WWamich <admiral3233@mail.ru>
2025-10-17 16:14:53 +0300

    undef -> undefined

--- src/Task25/Solution.hs
+++ src/Task25/Solution.hs
@@ -43,7 +43,7 @@ findDigitsFib235 :: Int -> Int
 findDigitsFib235 n =
   let indexed = zip [0 ..] fibs
    in case dropWhile (\(_, x) -> x < 10 ^ (n - 1)) indexed of
-      ((idx, _):_) -> fst (idx, undef)
+      ((idx, _):_) -> fst (idx, undefined)
       []           -> error "пролёт"
 
 -- спец.синтаксис для циклов
