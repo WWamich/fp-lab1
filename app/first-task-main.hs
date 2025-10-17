6bc95cc566d68edaaed9595dc32005a596b93e60
WWamich <admiral3233@mail.ru>
2025-10-17 16:33:34 +0300

    first-task-main updated to safety

--- app/first-task-main.hs
+++ app/first-task-main.hs
@@ -11,7 +11,14 @@ main = do
   print $ findLCM1_2 [1 .. 20]
   putStrLn "\n модульная через свёртку fold"
   print $ findLCM2 [1 .. 20]
-  putStrLn "\n map + беск.списки"
-  print $ head $ filter findLCM345 (map (* 2520) [1 ..])
-  putStrLn "\n Cпец. синтаксис циклов(шаг)"
-  print $ head $ filter findLCM345 [2520, 5040 ..]
+  putStrLn "\n[4] map + беск.списки"
+    let result1 = filter findLCM345 (map (*2520) [1..])
+    case result1 of
+      (x:_) -> print x
+      []    -> putStrLn "ошибочка"
+
+    putStrLn "\n[5] Cпец. синтаксис циклов(шаг)"
+    let result2 = filter findLCM345 [2520, 5040..]
+    case result2 of
+      (x:_) -> print x
+      []    -> putStrLn "ошибочка"
